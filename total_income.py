84da5d5201cd242bc5a6c007ad52c36a7b823c24
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-23 13:57:22 +1000

    answer questions 6 and 7.

--- total_income.py
+++ total_income.py
@@ -12,7 +12,9 @@ def main():
     for month in range(1, number_of_months + 1):
         income = float(input("Enter income for month {}: ".format(str(month))))
         incomes.append(income)
+    generate_report(number_of_months, incomes)
 
+def generate_report(number_of_months, incomes):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
