fd06065ac4f010e875f5a72cd5412c6627ecc68a
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-23 21:52:43 +1000

    add first take at Quick Pick program - which is very wrong but is starting to make me laugh so i'm going to post this edition to refer to.

--- quickpick.py
+++ quickpick.py
@@ -13,10 +13,23 @@ for i in range(number_of_games):
     numbers = []
     # generate the list of 6 unique random numbers
     for x in range(6):
-        numbers.append(randint(MIN_NUMBER - 1, MAX_NUMBER))
+        number = randint(MIN_NUMBER, MAX_NUMBER + 1)
+        print("first rand is {}".format(number))
+        if number not in numbers:
+            numbers.append(number)
+            print("from the if loop", str(number))
+        else:
+            number = randint(MIN_NUMBER, MAX_NUMBER + 1)
+            print("Priming read is {}".format(number))
+            while number not in numbers:
+                numbers.append(number)
+                number = randint(MIN_NUMBER, MAX_NUMBER + 1)
+                print("from the while  loop", str(number))
+
+        # numbers.append([(randint(MIN_NUMBER - 1, MAX_NUMBER)) for number in numbers if number not in numbers])
     print(numbers)
 print()
 
 
-    # numbers = [random.randint(number) for number in range(6)]
+    # numbers = [numbers.append(randint(MIN_NUMBER - 1, MAX_NUMBER)) for number in numbers if number not in numbers]
     # print(numbers)
