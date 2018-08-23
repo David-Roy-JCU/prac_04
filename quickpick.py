2a78cfcbbaf42f527d6da1f52c1c9b89897b7a9c
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-23 22:06:23 +1000

    update code that actually works - giving the results as expected

--- quickpick.py
+++ quickpick.py
@@ -11,23 +11,19 @@ number_of_games = int(input("How many Quick Picks? "))
 
 for i in range(number_of_games):
     numbers = []
-    # generate the list of 6 unique random numbers
     for x in range(6):
-        number = randint(MIN_NUMBER, MAX_NUMBER + 1)
-        print("first rand is {}".format(number))
+        number = randint(MIN_NUMBER, MAX_NUMBER)
+        print("{} random is {}".format(x + 1, number))
         if number not in numbers:
             numbers.append(number)
-            print("from the if loop", str(number))
+            print("appended by the if loop", str(number))
         else:
-            number = randint(MIN_NUMBER, MAX_NUMBER + 1)
-            print("Priming read is {}".format(number))
-            while number not in numbers:
-                numbers.append(number)
-                number = randint(MIN_NUMBER, MAX_NUMBER + 1)
-                print("from the while  loop", str(number))
+            while number in numbers:
+                number = randint(MIN_NUMBER, MAX_NUMBER)
+            numbers.append(number)
+            print("derived from the while loop", str(number))
 
-        # numbers.append([(randint(MIN_NUMBER - 1, MAX_NUMBER)) for number in numbers if number not in numbers])
-    print(numbers)
+    print(sorted(numbers))
 print()
 
 
