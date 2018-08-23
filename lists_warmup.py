21e54bfdebe582adefffc6973f8b3d4cb852d564
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-23 13:36:23 +1000

    create warmup file

---
+++ lists_warmup.py
@@ -0,0 +1,32 @@
+"""
+prac 04 - Lists
+
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 3 /
+# 2 /
+# 1 /
+# ? x  slices the last value off and returns the list
+# ? x andswer is 1.  it defines a range? look this up!
+# True /
+# False /
+# False /
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3] /
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+numbers[2:]
+9 in numbers
+
