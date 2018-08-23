59530d01caf22c536c4bc2a29e40fbc81537512d
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-08-23 23:06:45 +1000

    provide cleaner code with results formatted as required.

--- quickpick.py
+++ quickpick.py
@@ -13,19 +13,13 @@ for i in range(number_of_games):
     numbers = []
     for x in range(6):
         number = randint(MIN_NUMBER, MAX_NUMBER)
-        print("{} random is {}".format(x + 1, number))
         if number not in numbers:
             numbers.append(number)
-            print("appended by the if loop", str(number))
         else:
             while number in numbers:
                 number = randint(MIN_NUMBER, MAX_NUMBER)
             numbers.append(number)
-            print("derived from the while loop", str(number))
-
-    print(sorted(numbers))
-print()
-
-
-    # numbers = [numbers.append(randint(MIN_NUMBER - 1, MAX_NUMBER)) for number in numbers if number not in numbers]
-    # print(numbers)
+    numbers_sorted = sorted(numbers)
+    for i in range(len(numbers_sorted)):
+        print("{:>3}".format(numbers_sorted[i]), end = '')
+    print()
